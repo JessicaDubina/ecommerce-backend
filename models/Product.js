9d4d5a6c56dfcd1887ed4468492efa38b6a1e5fc
jdub <jessicaclaing@gmail.com>
2024-02-18 10:14:52 -0800

    setup models and relationships

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,33 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      //validates value is decimal
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      //validates value is numeric
+      validate: {
+        isNumeric: true,
+      }
+    }
   },
   {
     sequelize,
